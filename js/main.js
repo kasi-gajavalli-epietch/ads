a35ebaad9ba6896bbed6bcd0a0540bceb090e156
Kasi GAJAVALLI <kasi.gajavalli@gmail.com>
2023-06-15 21:42:10 +0200

    glass & dark working

--- js/main.js
+++ js/main.js
@@ -139,52 +139,7 @@ styleSwitcherToggle();
 /* -------------------------------------------
  theme colors
 ----------------------------------------------*/
-function themeColors() {
-  const colorStyle = document.querySelector(".js-color-style"),
-    themeColorsContainer = document.querySelector(".js-theme-colors");
 
-  themeColorsContainer.addEventListener("click", ({ target }) => {
-    if (target.classList.contains("js-theme-color-item")) {
-      localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
-      setColor();
-    }
-  });
-
-  function setColor() {
-    let path = colorStyle.getAttribute("href").split("/");
-    path = path.slice(0, path.length - 1);
-    colorStyle.setAttribute(
-      "href",
-      path.join("/") + "/" + localStorage.getItem("color") + ".css"
-    );
-
-    if (document.querySelector(".js-theme-color-item.active")) {
-      document
-        .querySelector(".js-theme-color-item.active")
-        .classList.remove("active");
-    }
-    document
-      .querySelector(
-        "[data-js-theme-color=" + localStorage.getItem("color") + "]"
-      )
-      .classList.add("active");
-  }
-
-  if (localStorage.getItem("color") !== null) {
-    setColor();
-  } else {
-    const defaultColor = colorStyle
-      .getAttribute("href")
-      .split("/")
-      .pop()
-      .split(".")
-      .shift();
-    document
-      .querySelector("[data-js-theme-color=" + defaultColor + "]")
-      .classList.add("active");
-  }
-}
-themeColors();
 
 /* theme light & dark mode */
 function themeLightDark() {
@@ -254,7 +209,7 @@ themeGlassEffect();
 /* why choose */
 
 const bannerText = document.getElementById("banner-text");
-const cards = document.que(".wcu-card");
+const cards = document.querySelectorAll(".wcu-card");
 
 // Options for the Intersection Observer
 const options = {
