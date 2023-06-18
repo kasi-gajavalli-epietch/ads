212ad6cc7229c9db1a9ed0fc22ca2c6c15edcc5c
Kasi GAJAVALLI <kasi.gajavalli@gmail.com>
2023-06-18 22:16:44 +0200

    hh

--- js/main.js
+++ js/main.js
@@ -123,12 +123,12 @@ function scrollToTop() {
 
 /* ------------------------------------ 
   Style Switcher
------------------------------------------*/
-function styleSwitcherToggle() {
+-----------------------------------------*/ 
+function styleSwitcherToggle(){
   const styleSwitcher = document.querySelector(".js-style-switcher"),
-    styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
+  styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
 
-  styleSwitcherToggler.addEventListener("click", function () {
+  styleSwitcherToggler.addEventListener("click", function(){
     styleSwitcher.classList.toggle("open");
     this.querySelector("i").classList.toggle("fa-times");
     this.querySelector("i").classList.toggle("fa-cog");
@@ -137,105 +137,102 @@ function styleSwitcherToggle() {
 styleSwitcherToggle();
 
 /* -------------------------------------------
- theme colors
-----------------------------------------------*/
-
-
-/* -------------------------------------------
- theme colors
+theme colors
 ----------------------------------------------*/ 
 function themeColors(){
-  const colorStyle = document.querySelector(".js-color-style"),
-  themeColorsContainer = document.querySelector(".js-theme-colors");
+ const colorStyle = document.querySelector(".js-color-style"),
+ themeColorsContainer = document.querySelector(".js-theme-colors");
 
-  themeColorsContainer.addEventListener("click", ({target}) => {
-    if(target.classList.contains("js-theme-color-item")){
-      localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
-      setColor();
-    }
-  });
+ themeColorsContainer.addEventListener("click", ({target}) => {
+   if(target.classList.contains("js-theme-color-item")){
+     localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
+     setColor();
+   }
+ });
 
-  function setColor(){
-    let path = colorStyle.getAttribute("href").split("/");
-    path = path.slice(0, path.length-1);
-    colorStyle.setAttribute("href", path.join("/") + "/" + localStorage.getItem("color") + ".css");
+ function setColor(){
+   let path = colorStyle.getAttribute("href").split("/");
+   path = path.slice(0, path.length-1);
+   colorStyle.setAttribute("href", path.join("/") + "/" + localStorage.getItem("color") + ".css");
 
-    if(document.querySelector(".js-theme-color-item.active")){
-      document.querySelector(".js-theme-color-item.active").classList.remove("active");
-    }
-    document.querySelector("[data-js-theme-color=" + localStorage.getItem("color") + "]").classList.add("active");
-  }
+   if(document.querySelector(".js-theme-color-item.active")){
+     document.querySelector(".js-theme-color-item.active").classList.remove("active");
+   }
+   document.querySelector("[data-js-theme-color=" + localStorage.getItem("color") + "]").classList.add("active");
+ }
 
-  if(localStorage.getItem("color") !== null){
-    setColor();
-  } 
+ if(localStorage.getItem("color") !== null){
+   setColor();
+ }  
+ else{
+   const defaultColor = colorStyle.getAttribute("href").split("/").pop().split(".").shift();
+   document.querySelector("[data-js-theme-color=" + defaultColor + "]").classList.add("active");
+ }
 }
 themeColors();
 
 /* theme light & dark mode */ 
 function themeLightDark(){
-  const darkModeCheckbox = document.querySelector(".js-dark-mode");
+ const darkModeCheckbox = document.querySelector(".js-dark-mode");
 
-  darkModeCheckbox.addEventListener("click", function() {
-   if(this.checked){
-     localStorage.setItem("theme-dark", "true");
+ darkModeCheckbox.addEventListener("click", function() {
+  if(this.checked){
+    localStorage.setItem("theme-dark", "true");
+  }
+  else{
+   localStorage.setItem("theme-dark", "false");
+  }
+  themeMode();
+ });
+
+ function themeMode(){
+   if(localStorage.getItem("theme-dark") === "false"){
+     document.body.classList.remove("t-dark");
    }
    else{
-    localStorage.setItem("theme-dark", "false");
+     document.body.classList.add("t-dark");
    }
+ }
+
+ if(localStorage.getItem("theme-dark") !== null){
    themeMode();
-  });
-
-  function themeMode(){
-    if(localStorage.getItem("theme-dark") === "false"){
-      document.body.classList.remove("t-dark");
-    }
-    else{
-      document.body.classList.add("t-dark");
-    }
-  }
-
-  if(localStorage.getItem("theme-dark") !== null){
-    themeMode();
-  }
-  if(document.body.classList.contains("t-dark")){
-    darkModeCheckbox.checked = true;
-  }
+ }
+ if(document.body.classList.contains("t-dark")){
+   darkModeCheckbox.checked = true;
+ }
 }
 themeLightDark();
 
 /* theme glass effect */ 
-function themeGlassEffect(){
-  const glassEffectCheckbox = document.querySelector(".js-glass-effect"),
-  glassStyle = document.querySelector(".js-glass-style");
+function themeGlassEffect() {
+  const glassEffectCheckbox = document.querySelector(".js-glass-effect");
+  const glassStyle = document.querySelector(".js-glass-style");
 
-  glassEffectCheckbox.addEventListener("click", function(){
-   if(this.checked){
-     localStorage.setItem("glass-effect", "true");
-   }
-   else{
-    localStorage.setItem("glass-effect", "false");
-   }
-   glass();
+  glassEffectCheckbox.addEventListener("click", function () {
+    if (this.checked) {
+      enableGlass();
+    } else {
+      disableGlass();
+    }
   });
 
-  function glass(){
-    if(localStorage.getItem("glass-effect") === "true"){
-      glassStyle.removeAttribute("disabled");
-    }
-    else{
-      glassStyle.disabled = true;
-    }
+  function enableGlass() {
+    glassStyle.removeAttribute("disabled");
   }
-  if(localStorage.getItem("glass-effect") !== null){
-    glass();
+
+  function disableGlass() {
+    glassStyle.disabled = true;
   }
-  
-  if(!glassStyle.hasAttribute("disabled")){
+
+  enableGlass(); // Enable glass theme by default
+
+  if (!glassStyle.hasAttribute("disabled")) {
     glassEffectCheckbox.checked = true;
   }
 }
+
 themeGlassEffect();
+
 
 /* why choose */
 
