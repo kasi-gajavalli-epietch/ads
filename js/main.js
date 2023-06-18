31c9a65c6048dd8f0d684a084165cddbae042999
Kasi GAJAVALLI <kasi.gajavalli@gmail.com>
2023-06-19 00:31:13 +0200

    final-1

--- js/main.js
+++ js/main.js
@@ -264,3 +264,33 @@ const observer = new IntersectionObserver(handleIntersect, options);
 cards.forEach((card) => {
   observer.observe(card);
 });
+
+const form = document.querySelector("form"),
+  statusTxt = form.querySelector(".button-area span");
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://ads-ebon.vercel.app/message.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = xhr.response;
+      if (
+        response.indexOf("required") != -1 ||
+        response.indexOf("valid") != -1 ||
+        response.indexOf("failed") != -1
+      ) {
+        statusTxt.style.color = "red";
+      } else {
+        form.reset();
+        setTimeout(() => {
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  };
+  let formData = new FormData(form);
+  xhr.send(formData);
+};
