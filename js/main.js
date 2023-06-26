f790d27e5194a3465c4c79969194a98a177ede26
Kasi GAJAVALLI <kasi.gajavalli@gmail.com>
2023-06-26 06:34:51 +0200

    hh

--- js/main.js
+++ js/main.js
@@ -8,24 +8,6 @@ window.addEventListener("load", () => {
   }, 600);
 });
 
-/* ------------------------------------------
- course preview video
----------------------------------------------*/
-/*function coursePreviewVideo(){
-    const coursePreviewModal = document.querySelector(".js-course-preview-modal");
-    if(coursePreviewModal){ /*if the element exists*/
-/*coursePreviewModal.addEventListener("shown.bs.modal", function(){
-           this.querySelector(".js-course-preview-video").play();
-           this.querySelector(".js-course-preview-video").currentTime = 0;
-        });
-
-        coursePreviewModal.addEventListener("hide.bs.modal", function() {
-            this.querySelector(".js-course-preview-video").pause();
-        });
-    }
-}
-coursePreviewVideo();*/
-
 /* ----------------------------- 
  header menu 
  -------------------------------*/
@@ -121,130 +103,35 @@ function scrollToTop() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-/* ------------------------------------ 
-  Style Switcher
------------------------------------------*/
-function styleSwitcherToggle() {
-  const styleSwitcher = document.querySelector(".js-style-switcher"),
-    styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
-
-  styleSwitcherToggler.addEventListener("click", function () {
-    styleSwitcher.classList.toggle("open");
-    this.querySelector("i").classList.toggle("fa-times");
-    this.querySelector("i").classList.toggle("fa-cog");
-  });
-}
-styleSwitcherToggle();
-
-/* -------------------------------------------
-theme colors
-----------------------------------------------*/
-function themeColors() {
-  const colorStyle = document.querySelector(".js-color-style"),
-    themeColorsContainer = document.querySelector(".js-theme-colors");
-
-  themeColorsContainer.addEventListener("click", ({ target }) => {
-    if (target.classList.contains("js-theme-color-item")) {
-      localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
-      setColor();
-    }
-  });
-
-  function setColor() {
-    let path = colorStyle.getAttribute("href").split("/");
-    path = path.slice(0, path.length - 1);
-    colorStyle.setAttribute(
-      "href",
-      path.join("/") + "/" + localStorage.getItem("color") + ".css"
-    );
-
-    if (document.querySelector(".js-theme-color-item.active")) {
-      document
-        .querySelector(".js-theme-color-item.active")
-        .classList.remove("active");
-    }
-    document
-      .querySelector(
-        "[data-js-theme-color=" + localStorage.getItem("color") + "]"
-      )
-      .classList.add("active");
-  }
-
-  if (localStorage.getItem("color") !== null) {
-    setColor();
-  } else {
-    const defaultColor = colorStyle
-      .getAttribute("href")
-      .split("/")
-      .pop()
-      .split(".")
-      .shift();
-    document
-      .querySelector("[data-js-theme-color=" + defaultColor + "]")
-      .classList.add("active");
-  }
-}
-themeColors();
-
-/* theme light & dark mode */
-function themeLightDark() {
+/* Theme Dark Mode */
+function themeDarkMode() {
   const darkModeCheckbox = document.querySelector(".js-dark-mode");
 
-  darkModeCheckbox.addEventListener("click", function () {
+  darkModeCheckbox.addEventListener("change", function () {
     if (this.checked) {
+      document.body.classList.add("t-dark");
       localStorage.setItem("theme-dark", "true");
     } else {
+      document.body.classList.remove("t-dark");
       localStorage.setItem("theme-dark", "false");
     }
-    themeMode();
   });
 
-  function themeMode() {
-    if (localStorage.getItem("theme-dark") === "false") {
-      document.body.classList.remove("t-dark");
-    } else {
-      document.body.classList.add("t-dark");
-    }
-  }
-
-  if (localStorage.getItem("theme-dark") !== null) {
-    themeMode();
-  }
-  if (document.body.classList.contains("t-dark")) {
+  if (localStorage.getItem("theme-dark") === "true") {
     darkModeCheckbox.checked = true;
+    document.body.classList.add("t-dark");
   }
 }
-themeLightDark();
+themeDarkMode();
 
-/* theme glass effect */
-function themeGlassEffect() {
-  const glassEffectCheckbox = document.querySelector(".js-glass-effect");
+/* Theme Glass Effect */
+function enableGlassEffect() {
   const glassStyle = document.querySelector(".js-glass-style");
-
-  glassEffectCheckbox.addEventListener("click", function () {
-    if (this.checked) {
-      enableGlass();
-    } else {
-      disableGlass();
-    }
-  });
-
-  function enableGlass() {
-    glassStyle.removeAttribute("disabled");
-  }
-
-  function disableGlass() {
-    glassStyle.disabled = true;
-  }
-
-  enableGlass(); // Enable glass theme by default
-
-  if (!glassStyle.hasAttribute("disabled")) {
-    glassEffectCheckbox.checked = true;
-  }
+  glassStyle.removeAttribute("disabled");
 }
 
-themeGlassEffect();
+enableGlassEffect(); // Enable glass effect by default
+
 
 /* why choose */
 
